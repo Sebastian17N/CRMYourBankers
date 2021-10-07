67b7d7c3d8890002fcb0bf23dfeb147b599a4f79
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2021-10-07 15:52:46 +0200

    added property in LoanApplicationDetailsVievModel

--- CRMYourBankers/Models/LoanApplication.cs
+++ CRMYourBankers/Models/LoanApplication.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace CRMYourBankers.Models
 {
-    class LoanApplication
+    public class LoanApplication
     {        
         public int AmountRequested { get; set; }
         public int AmountReceived { get; set; }
         public int ClientCommission { get; set; }
         public string TasksToDo { get; set; }
-
-        //public bool Validate()
-        //{
-        //    return
-
-        //}
-
 
 
     }
--- CRMYourBankers/ViewModels/LoanApplicationDetailsVievModel.cs
+++ CRMYourBankers/ViewModels/LoanApplicationDetailsVievModel.cs
@@ -1,17 +1,34 @@
 ﻿using CRMYourBankers.Models;
 using CRMYourBankers.ViewModels.Base;
 using System;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CRMYourBankers.ViewModels
 {
-    class LoanApplicationDetailsVievModel : TabBaseViewModel
+    public class LoanApplicationDetailsVievModel : TabBaseViewModel
     {
         public List<LoanApplication> LoanApplications { get; set; }
         public List<Bank> Banks { get; set; }
+
+        public int AmountRequestedText { get; set; }
+        public int AmountReceivedText { get; set; }
+        public int ClientCommissionText { get; set; }
+        public string TasksToDoText { get; set; }
+
+        public ICommand SaveButtonCommand { get; set; }
+        public ICommand CancelButtonCommand { get; set; }
+
+        public LoanApplicationDetailsVievModel(Messenger tabMessenger, List<LoanApplication> loanApplications) : base(tabMessenger)
+        {
+            LoanApplication = loanApplications;
+
+        }
 
     }
 }
