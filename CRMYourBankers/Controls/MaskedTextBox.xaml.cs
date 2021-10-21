9c2a41049a229ed8ef1a27593cf08b35ec203109
Zieluowaty <zielu.yabi@gmail.com>
2021-10-21 15:44:54 +0200

    Added empty textbox.

---
+++ CRMYourBankers/Controls/MaskedTextBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CRMYourBankers.Controls
+{
+    public partial class MaskedTextBox : TextBox
+    {
+        public MaskedTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
