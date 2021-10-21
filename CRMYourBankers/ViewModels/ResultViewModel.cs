d03a2265efbf74eb07af25c6473db5dcd1a8b6b3
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2021-10-21 14:48:11 +0200

    added Result view and viewModel

---
+++ CRMYourBankers/ViewModels/ResultViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CRMYourBankers.Database;
+using CRMYourBankers.Messages;
+using CRMYourBankers.Models;
+using CRMYourBankers.ViewModels.Base;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CRMYourBankers.ViewModels
+{
+    public class ResultViewModel : TabBaseViewModel
+    {
+        public ICommand DetailsScreenOpenHandler { get; set; }
+        public YourBankersContext Context { get; set; }
+
+        public ResultViewModel(Messenger messenger, YourBankersContext context) : base(messenger)
+        {
+            Context = context;
+            RegisterCommands();
+        }
+        
+        public void RegisterCommands()
+        {
+            DetailsScreenOpenHandler = new RelayCommand(() =>
+            {
+                TabMessenger.Send(new TabChangeMessage
+                {
+                    TabName = "Result"
+                });
+            });
+        }
+
+    }
+}
---
+++ CRMYourBankers/Views/ResultView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CRMYourBankers.Views
+{
+    /// <summary>
+    /// Interaction logic for ResultView.xaml
+    /// </summary>
+    public partial class ResultView : UserControl
+    {
+        public ResultView()
+        {
+            InitializeComponent();
+        }
+    }
+}
