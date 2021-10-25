839086729e98ce3101d3915b21199ee88152482d
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2021-10-25 17:43:33 +0200

    added % in resoultView

--- CRMYourBankers/ViewModels/ResultViewModel.cs
+++ CRMYourBankers/ViewModels/ResultViewModel.cs
@@ -21,15 +21,8 @@ namespace CRMYourBankers.ViewModels
         public ICommand DetailsScreenOpenHandler { get; set; }
         public YourBankersContext Context { get; set; }
         public LoanApplication SelectedLoanApplication { get; set; }
-
-        public List<string> YesNoList = new()
-        {
-            "Tak",
-            "Nie"
-        };
-
+                
         public dynamic DataGridData { get; set; }
-
         public ObservableCollection<MonthSummary> MonthSummaries { get; set; }
 
         private MonthSummary _selectedMonthSummary;
@@ -41,18 +34,24 @@ namespace CRMYourBankers.ViewModels
                 _selectedMonthSummary = value;
                 NotifyPropertyChanged("SelectedMonthSummary");
                 NotifyPropertyChanged("ActualScore");
-               
+                NotifyPropertyChanged("RealizedScore");
+                // jeśli kliknę w view w miesiąc to on wypełnia Value, potem SelectedMonthSummary
+                // potem _selectedMonthSummary a NotifyPropertyChanged odświeża watości na view
                 RefreshData();
             }
         }
 
         public string ActualScore =>
-            SelectedMonthSummary != null ?
+            SelectedMonthSummary != null ? ActualScoreValue.ToString() : "wybierz miesiąc";
+        public int ActualScoreValue =>
             Context
                 .LoanApplications
                 .Where(loan => loan.LoanStartDate.Month == SelectedMonthSummary.Month.Month)
-                .Sum(loan => loan.AmountReceived).ToString() :
-            "wybierz miesiąc";
+                .Sum(loan => loan.AmountReceived).Value;
+        //to jest tylko getter, jeśli nie ma settera to na view musi być ustalone mode = one 
+        //ponieważ dzięki temu kontrolki wiedzą, że to jest kmunikacja jednokierunkowa, view model na view
+        public double RealizedScore => SelectedMonthSummary != null ?
+            Math.Round(ActualScoreValue*100 / (double)SelectedMonthSummary.EstimatedTarget, 2) : 0;
 
         public ResultViewModel(Messenger messenger, YourBankersContext context) : base(messenger)
         {
@@ -87,7 +86,6 @@ namespace CRMYourBankers.ViewModels
                         ).ToList();
 
                 NotifyPropertyChanged("DataGridData");
-                NotifyPropertyChanged("YesNoList");
             }
         }
         public void RegisterCommands()
