c79b9dde7dc57b61a4e88c8e440566bd5fa0d9c1
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2021-11-15 15:57:34 +0100

    added multibroker enum list to loan application details

---
+++ CRMYourBankers/Enums/MultiBroker.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace CRMYourBankers.Enums
+{
+    public enum MultiBroker
+    {
+        [Description("Lendi")]
+        Lendi,
+        [Description("Open")]
+        Open,
+        [Description("Brak")]
+        None,
+        [Description("Credit Agricole")]
+        CA
+    }
+}
